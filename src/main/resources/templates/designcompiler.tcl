6ddbc91b3d7cf68a5b1a6b86ff004a60450e2a16
Norman Kluge <norman.kluge@hpi.de>
2018-12-04 18:49:09 +0100

    use compile instead of compile_ultra (compile_ultra generates all-zero circuits lately)

--- src/main/resources/templates/designcompiler.tcl
+++ src/main/resources/templates/designcompiler.tcl
@@ -1,18 +1,18 @@
 #
 # Copyright (C) 2017 - 2018 Norman Kluge
-# 
+#
 # This file is part of ASGwrapper-synctools.
-# 
+#
 # ASGwrapper-synctools is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
-# 
+#
 # ASGwrapper-synctools is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with ASGwrapper-synctools.  If not, see <http://www.gnu.org/licenses/>.
 #
@@ -77,7 +77,7 @@ if {$rvs == 0} {
 #+dc_tcl_uniquify_end+#
 
 #+dc_tcl_compile_begin+#
-set rvs [compile_ultra]
+set rvs [compile -ungroup_all -no_design_rule -map_effort high]
 if {$rvs == 0} {
 	#*dc_tcl_errorcode*#
 }
